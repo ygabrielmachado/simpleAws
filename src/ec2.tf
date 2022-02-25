a9976d790a30fe2c26a61f64bbbdcf51583862f3
Gabriel Machado <ygabrielmachado>
2022-02-25 02:09:50 -0300

    files update

--- src/ec2.tf
+++ src/ec2.tf
@@ -2,31 +2,34 @@
    most_recent = true
 
    filter {
-     name   = "name"
-     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+     name           = "name"
+     values         = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
    }
 
-   owners = ["099720109477"] # Ubuntu
+   owners           = ["099720109477"] # Ubuntu
  }
 
  resource "aws_instance" "ec2_web_public" {
-   ami           = data.aws_ami.ubuntu.id
-   instance_type = "t2.micro"
+   ami              = data.aws_ami.ubuntu.id
+   instance_type    = "t2.micro"
    subnet_id = aws_subnet.public_subnet.id
+   aws_security_group = aws.aws_security_group.ec2_group_ce.id
+   key_name         = "keypair"
 
    tags = {
-     Name = "A_public_instance"
-     JT_Name = "Gabriel Machado"
+     Name           = "A_public_instance"
+     Owner          = "Project simpleAWS"
    }
  }
 
   resource "aws_instance" "ec2_web_private" {
-   ami           = data.aws_ami.ubuntu.id
-   instance_type = "t2.micro"
-   subnet_id = aws_subnet.private_subnet.id
+   ami              = data.aws_ami.ubuntu.id
+   instance_type    = "t2.micro"
+   subnet_id        = aws_subnet.private_subnet.id
+   key_name         = "keypair"
 
    tags = {
-     Name = "A_private_instance"
-     JT_Name = "Gabriel Machado"
+     Name           = "A_private_instance"
+     Owner          = "Project simpleAWS"
    }
  }
